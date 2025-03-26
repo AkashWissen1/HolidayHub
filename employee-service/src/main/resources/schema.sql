200f8a70a5288f980a8a0bb0dcc668c08314b8fa
Giri-Prasath-V <giri.prasath@wissen.com>
2025-03-26 12:53:24 +0530

    First commit on hr service

--- employee-service/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE employees (
-	employee_id SERIAL PRIMARY KEY,
-	employee_name VARCHAR(100) NOT NULL,
-	designation VARCHAR(50) NOT NULL,
-	email VARCHAR(100) UNIQUE NOT NULL,
-	password VARCHAR(255) NOT NULL,
-	client_id BIGINT
-);
-
---
+++ hr-service/src/main/java/com/example/hrservice/controllers/HRController.java
@@ -0,0 +1,62 @@
+package com.example.hrservice.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.hrservice.dto.ClientAssignDto;
+import com.example.hrservice.entities.Employee;
+import com.example.hrservice.entities.Holiday;
+import com.example.hrservice.services.HRService;
+
+@RestController
+public class HRController {
+	
+	@Autowired
+	HRService hrservice;
+	
+	@GetMapping("/hr")
+	public ResponseEntity<List<Employee>> getEmployee()
+	{
+		return ResponseEntity.ok(hrservice.getEmployee());
+	}
+	
+	@GetMapping("/search/employee/{employeeId}")
+	public ResponseEntity<List<Holiday>> getHolidaysForEmployee(@PathVariable Long employeeId)
+	{
+		return ResponseEntity.ok(hrservice.getHolidaysForEmployee(employeeId));
+	}
+	
+	@GetMapping("/search/client/{clientName}")
+	public ResponseEntity<List<Holiday>> getHolidaysOfClient(@PathVariable String clientName)
+	{
+		return ResponseEntity.ok(hrservice.getHolidaysOfClient(clientName));
+	}
+	
+	@PutMapping("/assign")
+	public ResponseEntity<String> assignClientToEmployee(@RequestBody ClientAssignDto dto) 
+	{
+	    
+
+	    boolean isAssigned = hrservice.assignClientToEmployee(dto);
+
+	    if (isAssigned) 
+	    {
+	        return ResponseEntity.ok("Client assigned successfully");
+	    } else 
+	    {
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Employee or Client not found.");
+	    }
+	}
+
+
+}
